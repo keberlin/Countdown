affb051d2f5d1a7019c492dae442bc0e44411ff5
Keith Eberlin <keith.eberlin@yahoo.co.uk>
2021-03-28 13:54:28 +0100

    Removed the pre-compiled expression in favour of it being generated when needed.

--- countdown.py
+++ countdown.py
@@ -1,23 +1,92 @@
 import argparse
 
+ADDITION = "+"
+SUBTRACTION = "-"
+MULTIPLICATION = "*"
+DIVISION = "/"
+
+
+def oper_level(o):
+    if o == ADDITION or o == SUBTRACTION:
+        return 0
+    return 1
+
+
+class Perm:
+    def __init__(
+        self, total, count, number=None, left=None, right=None, operator=None,
+    ):
+        self.total = total
+        self.count = count
+        self.number = number
+        self.left = left
+        self.right = right
+        self.operator = operator
+
+    def bracket(self, p):
+        if p.number or oper_level(self.operator) == oper_level(p.operator):
+            return str(p)
+        return "(" + str(p) + ")"
+
+    def __str__(self):
+        if self.number:
+            return self.number
+        return f"{self.bracket(self.left)} {self.operator} {self.bracket(self.right)}"
+
 
 def oper(a, b):
     # Perform an addition between the 2 numbers
-    yield a[0] + b[0], f"({a[1]}+{b[1]})"
+    yield Perm(
+        total=a.total + b.total,
+        count=a.count + b.count,
+        left=a,
+        right=b,
+        operator=ADDITION,
+    )
     # Perform a subtraction between the 2 numbers
-    if a[0] > b[0]:
-        yield a[0] - b[0], f"({a[1]}-{b[1]})"
+    if a.total > b.total:
+        yield Perm(
+            total=a.total - b.total,
+            count=a.count + b.count,
+            left=a,
+            right=b,
+            operator=SUBTRACTION,
+        )
     # In both directions
-    if b[0] > a[0]:
-        yield b[0] - a[0], f"({b[1]}-{a[1]})"
+    if b.total > a.total:
+        yield Perm(
+            total=b.total - a.total,
+            count=a.count + b.count,
+            left=b,
+            right=a,
+            operator=SUBTRACTION,
+        )
     # Perform a multiplication between the 2 numbers
-    yield a[0] * b[0], f"({a[1]}*{b[1]})"
+    yield Perm(
+        total=a.total * b.total,
+        count=a.count + b.count,
+        left=a,
+        right=b,
+        operator=MULTIPLICATION,
+    )
     # Perform a division between the 2 numbers
-    if b[0] and a[0] % b[0] == 0:
-        yield a[0] // b[0], f"({a[1]}/{b[1]})"
+    if b.total and a.total % b.total == 0:
+        yield Perm(
+            total=a.total // b.total,
+            count=a.count + b.count,
+            left=a,
+            right=b,
+            operator=DIVISION,
+        )
     # In both directions
-    if a[0] and b[0] % a[0] == 0:
-        yield b[0] // a[0], f"({b[1]}/{a[1]})"
+    if a.total and b.total % a.total == 0:
+        yield Perm(
+            total=b.total // a.total,
+            count=a.count + b.count,
+            left=b,
+            right=a,
+            operator=DIVISION,
+        )
 
 
 def inverse(idx, sub):
@@ -40,14 +109,14 @@ def elems(numbers, idx):
 
 def perm(numbers):
     for n in numbers:
-        yield n, str(n)
+        yield Perm(total=n, count=1, number=str(n))
 
     for n in range(len(numbers) // 2):
         for idx1, idx2 in indexes(range(len(numbers)), n + 1):
             for a in perm(elems(numbers, idx1)):
                 for b in perm(elems(numbers, idx2)):
-                    for x, y in oper(a, b):
-                        yield x, y
+                    for p in oper(a, b):
+                        yield p
 
 
 parser = argparse.ArgumentParser(
@@ -63,14 +132,15 @@ numbers = args.numbers
 total = args.total[0]
 
 results = []
-for v in perm(numbers):
-    if v[0] == total:
-        results.append(v[1])
+for p in perm(numbers):
+    if p.total == total:
+        results.append(p)
 
 # Remove duplicates
-results = list(set(results))
+# results = list(set(results))
 
-results.sort(key=lambda x: len(x))
+# Sort in order of how many numbers were used
+results.sort(key=lambda x: x.count)
 
 for result in results:
     print(result)
